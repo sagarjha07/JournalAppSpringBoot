9225f97465926c2c33a41cf3dd9f9626b3b6c06e
SAGAR KUMAR JHA <jha8768@gmail.com>
2024-10-03 21:04:59 +0530

    SpringSecurity configuration changes

---
+++ src/main/java/com/spring/sagar/journalApp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.sagar.journalApp.repository;
+
+import com.spring.sagar.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    User findByUsername(String username);
+
+}
